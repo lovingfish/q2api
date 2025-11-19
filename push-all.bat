ecc202ebc1a8e6e23f2f3d74c0a5a84c04856e73
CassiopeiaCode <i@5i.gs>
2025-11-19 20:35:25 +0800

    删除多余的脚本

--- push-all.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM 双上游推送脚本
-REM 同时推送到GitHub和Codeberg仓库
-
-echo 正在推送到所有上游仓库...
-git push origin main
-echo 推送完成！
-pause
--- push-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# 双上游推送脚本
-# 同时推送到GitHub和Codeberg仓库
-
-echo "正在推送到所有上游仓库..."
-git push origin main
-echo "推送完成！"
